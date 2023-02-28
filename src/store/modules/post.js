3ea1beb29ade11ea57d34109d9506677041cfce4
CCriptexx <ccriptexx@gmail.com>
2023-02-28 15:00:30 +0300

    first commit

---
+++ src/store/modules/post.js
@@ -0,0 +1,34 @@
+export default {
+  state: {
+    posts: []
+  },
+  getters: {
+    allPosts(state) {
+      return state.posts
+    },
+    postsCount(state, getters) {
+      return getters.validPosts.length
+    },
+    validPosts(state) {
+      return state.posts.filter(p => {
+        return p.title && p.body
+      })
+    }
+  },
+  mutations: {
+    updatePosts(state, posts) {
+      state.posts = posts
+    },
+    createPost(state, newPost) {
+      state.posts.unshift(newPost)
+    }
+  },
+  actions: {
+    async fetchPosts(commit, limit = 3) {
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts?_limit=' + limit)
+      const posts = await res.json()
+
+      commit('updatePosts',  posts)
+    }
+  },
+}
